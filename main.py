1b988ed115157a9af428bd948c6943d140ddf8e2
Karthik <tarunkarthik999@gmail.com>
2018-03-29 15:10:28 +0530

    added code

--- main.py
+++ main.py
@@ -2,16 +2,25 @@ from keras.models import Sequential
 from keras.layers import Dense
 import numpy
 
-dataset = numpy.loadtxt("/Data/Data.csv", delimiter=",")
+dataset = numpy.loadtxt("Data_Jan.csv", delimiter=",")
 Max_Temp = dataset[...,0]
 Min_Temp = dataset[...,1]
 Rainfall = dataset[...,2]
 
+X = [Max_Temp,Min_Temp]
 
 #creating a model
 
 model = Sequential()
-model.add(Dense(2, input_dim=2, activation='linear'))
+model.add(Dense(1, input_dim=2, activation='linear'))
 model.add(Dense(4, activation='sigmoid'))
 model.add(Dense(1, activation='linear'))
+
+
+model.compile(loss = 'binary_crossentropy', optimizer = 'rmsprop', metrics = ['accuracy'])
+
+model.fit(X,Rainfall, epochs=12, batch_size=15)
+
+scores = model.evaluate(X ,Rainfall)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 

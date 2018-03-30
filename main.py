4328fc3cc0af2b2f44e068599561414742223f97
Karthik <tarunkarthik999@gmail.com>
2018-03-30 14:09:00 +0530

    Modified code

--- main.py
+++ main.py
@@ -4,20 +4,27 @@ import numpy
 
 dataset = numpy.loadtxt("Data_Jan.csv", delimiter=",")
 
-Rainfall = dataset[...,2]
+Rainfall = dataset[...,3]
 
-X = dataset[:,0:2]
+X = dataset[:,0:3]
+
+print(Rainfall)
+print(X)
 
 #creating a model
 
 model = Sequential()
 
-model.add(Dense(4,input_dim=2,activation='sigmoid'))
-model.add(Dense(1,activation='linear'))
+model.add(Dense(6,input_dim=3,activation='relu'))
+model.add(Dense(4,activation='relu'))
+model.add(Dense(1,activation='relu'))
 
-model.compile(optimizer='adam',loss='mean_squared_error',metrics=['mean_absolute_error'])
+model.compile(loss='mean_squared_error',optimizer='adam',metrics=['mean_absolute_error'])
 
-model.fit(X,Rainfall, epochs=100, batch_size=15)
+model.fit(X,Rainfall, epochs=2000, batch_size=15)
 
-scores = model.evaluate(X ,Rainfall)
-print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]))
+scores = model.evaluate(X, Rainfall)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+predictions = model.predict(X)
+print(predictions)

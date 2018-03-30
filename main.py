ce4939b1fa7f5150a1ae0f57decb2695fd5c0cde
Karthik <tarunkarthik999@gmail.com>
2018-03-30 23:32:47 +0530

    modified code

--- main.py
+++ main.py
@@ -15,13 +15,15 @@ print(X)
 
 model = Sequential()
 
-model.add(Dense(6,input_dim=3,activation='relu'))
-model.add(Dense(4,activation='relu'))
-model.add(Dense(1,activation='relu'))
+model.add(Dense(20,input_dim=3,activation='relu'))
+model.add(Dense(15,activation='relu'))
+model.add(Dense(10,activation='relu'))
+model.add(Dense(5,activation='relu'))
+model.add(Dense(1,activation='linear'))
 
-model.compile(loss='mean_squared_error',optimizer='adam',metrics=['mean_absolute_error'])
+model.compile(loss='mean_squared_error',optimizer='rmsprop',metrics=['accuracy'])
 
-model.fit(X,Rainfall, epochs=2000, batch_size=15)
+model.fit(X,Rainfall, epochs=10000, batch_size=15)
 
 scores = model.evaluate(X, Rainfall)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))

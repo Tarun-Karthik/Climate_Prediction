13fc4d4d8599c74688678ce3613caf71cbfe26fd
Karthik <tarunkarthik999@gmail.com>
2018-04-01 21:40:12 +0530

    Modified code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 import numpy
 
-dataset = numpy.loadtxt("Data_Jan.csv", delimiter=",")
+dataset = numpy.loadtxt("Data2.csv", delimiter=",")
 
 Rainfall = dataset[...,3]
 
@@ -18,15 +18,18 @@ model = Sequential()
 model.add(Dense(20,input_dim=3,activation='relu'))
 model.add(Dense(15,activation='relu'))
 model.add(Dense(10,activation='relu'))
-model.add(Dense(5,activation='relu'))
 model.add(Dense(1,activation='linear'))
 
 model.compile(loss='mean_squared_error',optimizer='rmsprop',metrics=['accuracy'])
 
-model.fit(X,Rainfall, epochs=10000, batch_size=15)
+model.fit(X,Rainfall, epochs=20000, batch_size=45)
 
 scores = model.evaluate(X, Rainfall)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
-predictions = model.predict(X)
-print(predictions)
+dataset2 = numpy.loadtxt("Data2.csv", delimiter=",")
+Z = dataset2[:,0:3]
+
+predictions = model.predict(Z)
+
+numpy.savetxt("predict.txt",predictions)

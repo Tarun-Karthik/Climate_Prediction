a3a3f5f801f68e4be75b4279211964d06226c8c0
Karthik <tarunkarthik999@gmail.com>
2018-04-02 08:24:18 +0530

    Modified code

--- main.py
+++ main.py
@@ -2,34 +2,27 @@ from keras.models import Sequential
 from keras.layers import Dense
 import numpy
 
-dataset = numpy.loadtxt("Data2.csv", delimiter=",")
 
-Rainfall = dataset[...,3]
 
-X = dataset[:,0:3]
-
-print(Rainfall)
-print(X)
-
-#creating a model
+month = [1,2,3,4,5,6,7,8,9,10,11,12]
 
 model = Sequential()
-
-model.add(Dense(20,input_dim=3,activation='relu'))
-model.add(Dense(15,activation='relu'))
-model.add(Dense(10,activation='relu'))
+model.add(Dense(5,input_dim=3,activation='sigmoid'))
+model.add(Dense(5,activation='sigmoid'))
 model.add(Dense(1,activation='linear'))
 
-model.compile(loss='mean_squared_error',optimizer='rmsprop',metrics=['accuracy'])
+for x in month:
+	dataset = numpy.loadtxt("./Training_Data/Month"+`x`+"/Train.csv", delimiter=",")
+	output = dataset[:,3]
+	input = dataset[:,0:3]
+	model.compile(optimizer='adam',loss='mean_absolute_error',metrics=['mean_absolute_error'])
+	model.fit(input,output, epochs=10000, batch_size=15)
+	scores = model.evaluate(input, output)
+	print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]))	
 
-model.fit(X,Rainfall, epochs=20000, batch_size=45)
 
-scores = model.evaluate(X, Rainfall)
-print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-
-dataset2 = numpy.loadtxt("Data2.csv", delimiter=",")
-Z = dataset2[:,0:3]
-
-predictions = model.predict(Z)
-
-numpy.savetxt("predict.txt",predictions)
+for x in month:
+	dataset2 = numpy.loadtxt("./Testing_Data/Month"+`x`+"/Test.csv", delimiter=",")
+	Z = dataset2[:,0:3]
+	predictions = model.predict(Z)
+	numpy.savetxt("./Results/Month"+`x`+".csv",predictions)

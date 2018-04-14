d3d59228e3ed41aace799bd9642f4c99e8ce5d48
Karthik M <mkarthik2597@gmail.com>
2018-04-14 17:32:43 +0530

    Improved results

--- BackProp.py
+++ BackProp.py
@@ -6,8 +6,8 @@ months = [1,2,3,4,5,6,7,8,9,10,11,12]
 Data = []
 
 model = Sequential()
-model.add(Dense(5,input_dim=3,activation='sigmoid'))
-model.add(Dense(5,activation='sigmoid'))
+model.add(Dense(10,input_dim=3,activation='sigmoid'))
+model.add(Dense(10,activation='sigmoid'))
 model.add(Dense(1,activation='linear'))
 
 for x in months:
@@ -18,8 +18,8 @@ Data = numpy.vstack(Data)
 input = Data[:,0:3]
 output = Data[:,3]
 
-model.compile(optimizer='adam',loss='mean_absolute_error',metrics=['mean_absolute_error'])
-model.fit(input,output, epochs=10000, batch_size=45)
+model.compile(optimizer='nadam',loss='mean_absolute_error',metrics=['mean_absolute_error'])
+model.fit(input,output, epochs=1500, batch_size=45)
 scores = model.evaluate(input, output)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]))
 
